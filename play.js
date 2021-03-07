372c7545722038a514d8f5df677ed86ff349555f
Sebastian Goscik <sebastian.goscik@live.co.uk>
2021-03-07 19:57:35 +0000

    first commit

---
+++ play.js
@@ -0,0 +1,135 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const { program } = require('commander');
+
+
+function time() {
+    const nanos = process.hrtime.bigint();
+    return nanos / 1000000n;
+}
+
+
+class Recording {
+
+    constructor(recording_directory) {
+        this.recording_directory = recording_directory;
+        let websocket_recording_path = path.join(recording_directory, 'websocket.txt')
+        let recording = fs.readFileSync(websocket_recording_path, 'utf8').split('\n');
+        this._recording = []
+        for (let line of recording) {
+            if (line != '') {
+                const space_index = line.indexOf(' ');
+                const delay_ms = Number(line.substring(0, space_index));
+                const message = line.substring(space_index + 1);
+                this._recording.push({ delay_ms, message });
+            }
+        }
+
+        this.api = null;
+        this._next_api_file = 0
+        this._getNextApi();
+
+        this._start_time = null;
+        this._end_time = null;
+    }
+
+    _getNextApi() {
+        let api_file_name = this._next_api_file + '.json';
+        let api_file_path = path.join(this.recording_directory, 'api', api_file_name);
+        this.api = fs.readFileSync(api_file_path);
+        this._next_api_file += 1;
+    }
+
+    getNextMessage() {
+        let { delay_ms, message } = this._recording.shift();
+        // check if we need to change the api
+        let msg = JSON.parse(message);
+        if (msg.name == 'mapLoaded')
+            this._getNextApi();
+
+        return { delay_ms, message };
+    }
+
+    send(ws) {
+        if (this._recording.length > 0) {
+            this._start_time = time();
+            const { delay_ms, message } = this.getNextMessage();
+
+            let nextRunInMillis;
+            if (this._end_time == null) {
+                nextRunInMillis = delay_ms;
+            } else {
+                nextRunInMillis = Math.max(0, delay_ms - Number(this._end_time - this._start_time));
+            }
+
+            setTimeout(function () {
+                ws.send(message);
+                this._end_time = time();
+                this.send(ws);
+            }.bind(this), nextRunInMillis)
+        }
+    }
+}
+
+
+function play(recording_path, port) {
+    let recording;
+
+    const wss = new WebSocket.Server({ noServer: true });
+    wss.on('connection', function connection(ws) {
+        recording = new Recording(recording_path);
+        recording.send(ws);
+    });
+
+    var server = http.createServer(function (request, response) {
+        const { method, url } = request;
+        // Set CORS headers
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Request-Method', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        response.setHeader('Access-Control-Allow-Headers', '*');
+        if (request.method === 'OPTIONS') {
+            response.writeHead(200);
+            response.end();
+            return;
+        }
+
+        if (url == '/api') {
+            response.write(recording.api);
+            response.end();
+            return;
+        } else {
+            target_file_path = path.join(recording_path, ...url.split('/'));
+            if (fs.existsSync(target_file_path)) {
+                response.write(fs.readFileSync(target_file_path));
+            } else {
+                console.error(`Missing file: ${target_file_path}`);
+            }
+            response.end();
+            return;
+        }
+    });
+
+    server.on('upgrade', function upgrade(request, socket, head) {
+        if (request.url === '/ws') {
+            wss.handleUpgrade(request, socket, head, function done(ws) {
+                wss.emit('connection', ws, request);
+            });
+        } else {
+            socket.destroy();
+        }
+    });
+
+    server.listen(port);
+}
+
+program
+    .option('-p --port', "The port on which to run the server", '8090')
+    .arguments('<recording_path>')
+    .action((recording_path, options, command) => {
+        console.log(`Playing '${recording_path}'`)
+        play(recording_path, options.port)
+    })
+    .parse(process.argv);
---
+++ record.js
@@ -0,0 +1,136 @@
+const WebSocket = require('ws');
+const fs = require('fs')
+const http = require('http');
+const path = require('path');
+const { program } = require('commander');
+
+function time() {
+    const nanos = process.hrtime.bigint();
+    return nanos / 1000000n;
+}
+
+function get_api_asset_paths(data) {
+    let to_download = [];
+    to_download.push(data.map.image);
+
+    for (let tile of data.map.tiles) {
+        to_download.push(tile.asset.resource);
+    }
+    to_download = to_download.concat(get_game_assets_paths(data.game));
+    return to_download
+}
+
+function get_game_assets_paths(data) {
+    let to_download = [];
+    for (let creature of data.creatures) {
+
+        if ('image' in creature) {
+            to_download.push(creature.image);
+        }
+        if ('token' in creature) {
+            to_download.push(creature.token);
+        }
+        if ('cachedToken' in creature) {
+            to_download.push(creature.cachedToken);
+        }
+    }
+    return to_download
+}
+
+function download_files(url, recording_path, files) {
+    const download_url = new URL(url.toString());
+    for (let filename of files) {
+        download_url.pathname = filename;
+        target_file_path = path.join(recording_path, ...filename.split('/'));
+
+        // Only download if we dont already have it
+        if (!fs.existsSync(target_file_path)) {
+
+            // Make sure the directory structure exists
+            fs.mkdirSync(path.dirname(target_file_path), { recursive: true });
+
+            // Download File
+            const file = fs.createWriteStream(target_file_path);
+            const request = http.get(download_url, function (response) {
+                response.pipe(file);
+            });
+        }
+    }
+}
+
+function save_api(url, recording_path, id) {
+    const api_url = new URL(url.toString());
+    api_url.pathname = '/api'
+    let request = http.get(api_url.toString(), function (response) {
+        let response_data = '';
+        response.setEncoding('utf8');
+
+        // Save the response json
+        response.on('data', function (chunk) {
+            response_data += chunk;
+        });
+
+        json_path = path.join(recording_path, 'api', id + '.json')
+
+        response.on("end", function () {
+            // Store the json to disk
+            fs.writeFileSync(json_path, response_data);
+
+            // Download missing files
+            download_files(url, recording_path, get_api_asset_paths(JSON.parse(response_data)));
+        });
+    })
+}
+
+function record(server, recording_path) {
+    let api_calls = 0;
+
+    if (fs.existsSync(recording_path)) {
+        throw new Error("Recording already exists");
+    } else {
+        fs.mkdirSync(recording_path);
+        fs.mkdirSync(path.join(recording_path, 'api'));
+    }
+
+    const url = new URL('http://' + server);
+
+    const recording = fs.createWriteStream(path.join(recording_path, 'websocket.txt'));
+    let previousTime = time();
+
+    save_api(url, recording_path, api_calls);
+    api_calls += 1;
+
+    const websocket_url = new URL(url.toString());
+    websocket_url.protocol = 'ws';
+    websocket_url.pathname = 'ws';
+    const ws = new WebSocket(websocket_url.toString());
+
+    ws.on('message', function incoming(data) {
+        let newTime = time();
+        recording.write((newTime - previousTime).toString());
+        recording.write(' ');
+        recording.write(data);
+        recording.write('\n');
+        msg = JSON.parse(data);
+        if (msg.name == 'mapLoaded') {
+            save_api(url, recording_path, api_calls);
+            api_calls += 1;
+        } else if (msg.name == 'gameUpdated') {
+            download_files(url, recording_path, get_game_assets_paths(msg.data));
+        } else if (msg.name == 'screenUpdated') {
+            if ('overlayImage' in msg.data) {
+                download_files(url, recording_path, [msg.data.overlayImage]);
+            }
+        }
+        previousTime = newTime;
+    });
+}
+
+program
+    .option('-o --output [path]', "Path where recording will be saved, defaults to current date & time", new Date().toISOString())
+    .arguments('<server>')
+    .action((server, options, command) => {
+        console.log(`Recording '${server}' to: ${options.output}`)
+        record(server, options.output)
+    })
+    .parse(process.argv);
